1e30d7d0ed2f8ad9737d2de3ecb0bff749281f7d
TaeYoonEom <_>
2025-04-28 14:05:25 +0900

    Initial commit

---
+++ src/main/java/com/example/carproject/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.carproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // templates/home.html로 이동
+    }
+}
